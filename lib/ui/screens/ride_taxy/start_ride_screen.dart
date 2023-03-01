81ded0d575266d0e1303ab23c5ebf60f54117ee2
Nimacocolabs <nimacocoalabs@gmail.com>
2023-03-01 10:05:58 +0530

    some changes in UI

--- lib/ui/screens/ride_taxy/start_ride_screen.dart
+++ lib/ui/screens/ride_taxy/start_ride_screen.dart
@@ -16,50 +16,13 @@ class _StartRideScreenState extends State<StartRideScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(),
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(15.0),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              SizedBox(height: 10,),
-              const Text("Search location where you need to go",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold)),
-              SizedBox(height: 10,),
-              Row(
-                children: [
-                  const Text("Where",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold)),
-                  SizedBox(width: 6,),
-                  AppTextBox(
-                    textFieldControl: _wherelocaton,
-                    hintText: 'Search location',
-                    keyboardType: TextInputType.text,
-                  ),
-                ],
-              ),
-              SizedBox(height: 10,),
-             Row(
-               children: [
-                 const Text("To",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold)),
-                 SizedBox(width: 6,),
-                 AppTextBox(
-                   textFieldControl: _tolocation,
-                   hintText: 'Search location',
-                   keyboardType: TextInputType.text,
-                 ),
-               ],
-             ),
-              SizedBox(height: 10,),
-              SizedBox(
-                width: 400,
-                height: 45,
-                child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: primaryColor,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10)),
-                    ),
-                    onPressed: (){}, child: const Text("Search",style: TextStyle(fontSize: 18),)),
-              ),
 
             ],
           ),
